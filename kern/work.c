avarzille/x15

commit 24fa58718ad2e895fd91484e5fc82f6b8868d777

kern/thread: improve thread_wakeup robustness

kern/work.c
@@ -259,9 +259,7 @@ work_pool_wakeup_manager(struct work_pool *pool)
         return;
     }
 
-    if ((pool->manager != NULL) && (pool->manager->thread != thread_self())) {
-        thread_wakeup(pool->manager->thread);
-    }
+    thread_wakeup(pool->manager->thread);
 }
 
 static void

kern/console.c
@@ -155,9 +155,7 @@ console_intr(struct console *console, const char *s)
         s++;
     }
 
-    if ((console->waiter != NULL) && (console->waiter != thread_self())) {
-        thread_wakeup(console->waiter);
-    }
+    thread_wakeup(console->waiter);
 
 out:
     spinlock_unlock(&console->lock);

kern/sref.c
@@ -976,8 +976,7 @@ sref_report_periodic_event(void)
 
     cache = sref_cache_get();
 
-    if (!sref_cache_is_registered(cache)
-        || (cache->manager == thread_self())) {
+    if (!sref_cache_is_registered(cache)) {
         return;
     }
 

kern/thread.c
@@ -2452,6 +2452,10 @@ thread_wakeup(struct thread *thread)
     struct thread_runq *runq;
     unsigned long flags;
 
+    if ((thread == NULL) || (thread == thread_self())) {
+        return;
+    }
+
     /*
      * There is at most one reference on threads that were never dispatched,
      * in which case there is no need to lock anything.

kern/thread.h
@@ -219,7 +219,8 @@ void thread_sleep(struct spinlock *interlock, const void *wchan_addr,
 /*
  * Schedule a thread for execution on a processor.
  *
- * No action is performed if the target thread is already in the running state.
+ * No action is performed if the target thread is NULL, the calling thread,
+ * or already in the running state.
  */
 void thread_wakeup(struct thread *thread);
 

commit 2238931c0288c6a08d5f6e52d00c154eabb9990d

kern/work: fix manager wakeup
Commit 24fa58718ad2e895fd91484e5fc82f6b8868d777 introduced a minor
regression by also removing a check on the manager of a work pool,
which is a pointer to a work_thread, and not directly a thread
structure.

kern/work.c
@@ -259,7 +259,9 @@ work_pool_wakeup_manager(struct work_pool *pool)
         return;
     }
 
-    thread_wakeup(pool->manager->thread);
+    if (pool->manager != NULL) {
+        thread_wakeup(pool->manager->thread);
+    }
 }
 
 static void